22d536d2da6f697bb1e4a5f6ca4a61ed771e2810
Kumar Gourav <63939999+KumarGudu@users.noreply.github.com>
2023-01-03 15:33:38 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,122 @@
+import img1 from "./Image/1.jpg";
+import img2 from "./Image/3.jpg";
+import img3 from "./Image/4.jpg";
+import img4 from "./Image/5.jpg";
+import img5 from "./Image/6.jpg";
+import img6 from "./Image/7.jpg";
+import img8 from "./Image/8.jpg";
+import img9 from "./Image/9.jpg";
+import img10 from "./Image/10.jpg";
+import img11 from "./Image/11.jpg";
+import img12 from "./Image/12.jpg";
+import img13 from "./Image/13.jpg";
+import img14 from "./Image/14.jpg";
+const list = [
+  {
+    id: 1,
+    title: "Camera",
+    author: "by Wonder Camera | 25 April 2018",
+    price: 89,
+    img: img1,
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Sun flower",
+    author: "by Maple Press  | 1 September 2020",
+    price: 98,
+    img: img2,
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Shirt",
+    author: "by Om  Editorial Team  | 25 November 2018",
+    price: 143,
+    img: img3,
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Kurti",
+    author: "by Wonder House  | 25 April 2018",
+    price: 57,
+    img: img4,
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Shirts",
+    author: "by Wonder House   | 1 January 2018",
+    price: 149,
+    img: img5,
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: "Shose",
+    author: "by Om  Editorial Team | 30 September 2020",
+    price: 135,
+    img: img6,
+    amount: 1,
+  },
+  {
+    id: 7,
+    title: "Pants ",
+    author: "by Om  Editorial Team  | 1 January 2021",
+    price: 693,
+    img: img8,
+    amount: 1,
+  },
+  {
+    id: 8,
+    title: "Lather Shose",
+    author: "by Wonder House   | 6 December 2020",
+    price: 289,
+    img: img9,
+    amount: 1,
+  },
+  {
+    id: 9,
+    title: "T - Shirt ",
+    author: "by Content Team at Target Publications",
+    price: 80,
+    img: img10,
+    amount: 1,
+  },
+  {
+    id: 10,
+    title: "Brain Activity  for Kids",
+    author: "by Maple Press | 1 September 2021",
+    price: 86,
+    img: img11,
+    amount: 1,
+  },
+  {
+    id: 11,
+    title: "Willy the Silly Panda",
+    author: "by Rebecca Smith  | 14 December 2021",
+    price: 120,
+    img: img12,
+    amount: 1,
+  },
+  {
+    id: 12,
+    title: "Kurti ",
+    author: "by Sudha Murty | 1 January 2015",
+    price: 157,
+    img: img13,
+    amount: 1,
+  },
+  {
+    id: 13,
+    title: "Grandma's Bag ",
+    author: "by Sudha Murty | 1 January 2015",
+    price: 157,
+    img: img14,
+    amount: 1,
+  },
+  
+];
+
+export default list;
